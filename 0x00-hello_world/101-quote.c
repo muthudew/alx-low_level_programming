90805f6066f2339959f1a893d44cabf185ca195d
muthudew <wmuthude@gmail.com>
2023-07-15 15:38:07 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece ot art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	write(2,
+	"and that piece ot art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
