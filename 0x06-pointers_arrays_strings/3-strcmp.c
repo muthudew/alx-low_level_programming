8881451083fd82ab95ce9fa0d5ee1ddae7fe424e
muthudew <wmuthude@gmail.com>
2023-07-27 10:54:21 -0700

    ./3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,20 +4,16 @@
  * @s1: input value
  * @s2: input value
  *
- * Return: s1[i] = s2[i]
+ * Return: 0
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
+	int b = 0;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	for (i = 0; s1[i] != '\0' && b == 0; i++)
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] = s2[i]);
-		}
-		i++;
+		b = s1[i] - s2[i];
 	}
-	return (0);
+	return (b);
 }
