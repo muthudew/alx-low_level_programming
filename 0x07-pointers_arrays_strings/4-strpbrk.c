86a88ca36a5bb7b6cfb47e0000dfa3f83e29e7fb
muthudew <wmuthude@gmail.com>
2023-07-31 08:07:30 -0700

    ./4-strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * _strpbrk - Entry point
+ * @s: input value
+ * @accept: input value
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int v;
+
+	while (*s)
+	{
+		for (v = 0; accept[v]; v++)
+		{
+			if (*s == accept[v])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
