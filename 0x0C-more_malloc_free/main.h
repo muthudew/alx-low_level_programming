ddd5911e7b16ed9b20a6364fb781e89eaddb0da2
muthudew <wmuthude@gmail.com>
2023-08-10 13:03:07 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,4 +4,10 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _putchar(char c);
+int main(int argc, char *argv[]);
+int is_digit(char *s);
+void error();
+int _strlen(char *s);
+
 #endif
